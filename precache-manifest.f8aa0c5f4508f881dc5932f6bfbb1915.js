d4a7395c3be649cead6f0d94bf02c6e3dc6090be
s8sachin <s.sachin94@gmail.com>
2019-01-03 12:18:13 +0530

    Updates

---
+++ precache-manifest.f8aa0c5f4508f881dc5932f6bfbb1915.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6329fa7ee51499f5a78",
+    "url": "/react-hello-world/static/css/main.4e823cb8.chunk.css"
+  },
+  {
+    "revision": "c6329fa7ee51499f5a78",
+    "url": "/react-hello-world/static/js/main.c6329fa7.chunk.js"
+  },
+  {
+    "revision": "efcb66dfb80e2645cdcc",
+    "url": "/react-hello-world/static/js/1.efcb66df.chunk.js"
+  },
+  {
+    "revision": "f5f6d7efd18cd5578267",
+    "url": "/react-hello-world/static/js/runtime~main.f5f6d7ef.js"
+  },
+  {
+    "revision": "5c95306255b99d456ead6578d46ac076",
+    "url": "/react-hello-world/index.html"
+  }
+];
